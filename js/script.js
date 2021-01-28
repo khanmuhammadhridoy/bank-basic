b9f4f0c9df8e46a8a5fb5c5cbba1ed54bf6bb52c
khanmuhammadhridoy <khanmuhammadhridoy@gmail.com>
2021-01-28 10:30:17 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('login').addEventListener("click",function(){
+    document.getElementById('loginArea').style.display="none";
+    document.getElementById('transitionArea').style.display="block";
+})
